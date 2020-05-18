bfd2d0f69977883f6d29de04f993e26684d137da
jws <canlion@naver.com>
2020-05-18 19:26:57 +0900

    Make subdirectory : od, depth_estimation

--- od/yolo_v1/yolo_config.py
+++ od/yolo_v1/yolo_config.py
@@ -1,8 +1,8 @@
 CFG = {
     'data_loader': {
         'dataset': 'voc',
-        'train_tfr_pattern': '/sandbox_datasets/voc_download/tfrecord/train/*',
-        'eval_tfr_pattern': '/sandbox_datasets/voc_download/tfrecord/val/*',
+        'train_tfr_pattern': '/sandbox/datasets/voc/train/*',
+        'eval_tfr_pattern': '/sandbox/datasets/voc/val/*',
     },
     'network': {
         'input_size': [448, 448, 3],
@@ -16,20 +16,20 @@ CFG = {
             'S': 7,
             'B': 2,
             'classes': 20,
-            'l2_decay': 5e-4,
+            'l2_decay': 5e-5,
             'lambda_coord': 5.,
             'lambda_noobj': .5,
         }
     },
     'train': {
-        'mixed_precision': True,
+        'mixed_precision': False,
         'total_step': 240000,
         'eval_step': 2000,
-        'batch_size': 16,
+        'train_batch_size': 8,
         'learning_rate': {
             'schedule': 'PiecewiseConstantDecay',
-            'boundaries': [120000, 180000],
-            'values': [2e-4, 2e-5, 2e-6],
+            'boundaries': [12000, 18000],
+            'values': [1e-4, 1e-5, 1e-6],
             'warmup_learning_rate': 1e-5,
             'warmup_steps': 2000,
         },
